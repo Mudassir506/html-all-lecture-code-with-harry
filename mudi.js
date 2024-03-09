ee727fdfcf42b62fb062083ded61ec1578122271
Mudassir506 <71308178+Mudassir506@users.noreply.github.com>
2024-03-09 21:27:19 +0500

    Add files via upload
    all lecture learn in html from youtube.

---
+++ mudi.js
@@ -0,0 +1,64 @@
+var firstName = "Mudassir"
+var lastName = "khan"
+
+console.log(firstName + " " + lastName);
+
+var n = 5;
+ n = n+1;
+console.log(n);
+
+var n= 5;
+n +=1;
+console.log(n);
+
+var num1 = 5;
+var num2 = 6;
+ console.log(num1+num2);
+
+ var num1 = 5;
+var num2 = 6;
+ console.log(num1+num2+9);
+
+ var num1 = 5;
+var num2 = 6;
+ console.log(num1-num2);
+ 
+ var num1 = 5;
+var num2 = 6;
+ console.log(num1*num2);
+ 
+ var num1 = 5;
+ var num2 = 6;
+  console.log(num1/num2);
+
+
+  var n = 6;
+  var a = 5;
+  console.log(n==a);
+
+  var n = 6;
+  console.log(5>n);
+
+  var n= 7;
+  console.log(5<n);
+
+  var n= 4;
+  console.log(5>=n);
+
+  var n= 5;
+  console.log(4<=n);
+
+  var n=5;
+  console.log(n);
+
+  var num1 = 20;
+  var num2 = 10;
+  console.log(num1+num2);
+
+  var car = "newcar"
+var car = "oldcar"
+console.log(car);
+
+var n = 4;
+
+
